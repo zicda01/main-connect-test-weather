cb09a5b6eda61b71572b05895bb5afc73c96ac4e
최수빈 <soobin1457@daum.net>
2025-09-05 17:14:12 +0900

    first commit

---
+++ main/src/main/java/com/example/main_connect_test_weather/MainConnectTestWeatherApplication.java
@@ -0,0 +1,13 @@
+package com.example.main_connect_test_weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainConnectTestWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainConnectTestWeatherApplication.class, args);
+	}
+
+}
---
+++ main/src/test/java/com/example/main_connect_test_weather/MainConnectTestWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.main_connect_test_weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainConnectTestWeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
