ce1c1ce8e5cc148cde54d7cfabc192d8b3534fcb
최수빈 <soobin1457@daum.net>
2025-09-10 14:43:40 +0900

    add weather controller

--- main/src/main/java/com/example/main_connect_test_weather/controller/WeatherController.java
+++ main/src/main/java/com/example/main_connect_test_weather/controller/WeatherController.java
@@ -1,5 +1,34 @@
 package com.example.main_connect_test_weather.controller;
 
+import com.example.main_connect_test_weather.service.WeatherService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+
+/**
+ * 날씨 정보 API 요청을 처리하는 restful 컨트롤러입니다.
+ */
+@RestController
+@RequestMapping("/weather")
 public class WeatherController {
 
+    private final WeatherService weatherService;
+
+    @Autowired
+    public WeatherController(WeatherService weatherService) {
+        this.weatherService = weatherService;
+    }
+
+    @GetMapping
+    public Mono<String> getCurrentWeather(
+            @RequestParam double lat,
+            @RequestParam double lon) {
+
+        return weatherService.getCurrentWeather(lat, lon);
+    }
 }
--- main/src/main/java/com/example/main_connect_test_weather/service/WeatherService.java
+++ main/src/main/java/com/example/main_connect_test_weather/service/WeatherService.java
@@ -1,5 +1,8 @@
 package com.example.main_connect_test_weather.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class WeatherService {
 
 }
